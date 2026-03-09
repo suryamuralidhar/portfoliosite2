16acd6b2165780bd5c0100588279bd00c0b539e6
suryamurali006 <suryamurali006@gmail.com>
2026-03-09 22:48:47 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+fetch("components/navbar.html")
+.then(response => response.text())
+.then(data => {
+document.getElementById("navbar").innerHTML = data;
+});
+
+fetch("components/footer.html")
+.then(response => response.text())
+.then(data => {
+document.getElementById("footer").innerHTML = data;
+});
